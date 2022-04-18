b70513eadcbec9cb42bb968dcfd46099a2147826
bitbauer <4582513+bitbauer@users.noreply.github.com>
2022-04-19 00:27:03 +0200

    ADD init code for aws sdk

--- aws-glasses.go
+++ aws-glasses.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"context"
+	"log"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
 
 func main() {
+	// Load the Shared AWS Configuration (~/.aws/config)
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	
+
 	fmt.Println("This command is not yet supported.")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/eu-central-1/aws-glasses
 
 go 1.17
+
+require (
+	github.com/aws/aws-sdk-go-v2/config v1.15.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.26.5 // indirect
+)
